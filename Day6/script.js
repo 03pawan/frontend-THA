e42a20f8ab96ade9d4640ffc4e1d762f5d20cc52
03pawan <pawankumar03012002@gmail.com>
2021-06-13 21:04:56 +0530

    Day6

---
+++ Day6/script.js
@@ -0,0 +1,29 @@
+
+// function is_array(arr){
+//    return Array.isArray(arr);
+// }
+
+// function array_Clone(arr){
+//     return arr.slice(0);
+// }
+
+// function first(arr,n=0){
+//     if(n>0)
+//         return arr.slice(0,n);
+//     else if(n==0)
+//         return arr[0];    
+//     else
+//         return arr.slice(0,n-1);
+// }
+
+// function joinArray(arr){
+//     console.log(arr.join(','));
+//     console.log(arr.join('+'));
+// }
+
+// function mode(arr){
+//     return arr.sort((a,b) =>
+//           arr.filter(v => v===a).length
+//         - arr.filter(v => v===b).length
+//     ).pop();
+// }
