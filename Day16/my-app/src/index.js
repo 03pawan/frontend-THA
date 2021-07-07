12791dcef50972e16f08207d9062aac1681cfe8e
03pawan <pawankumar03012002@gmail.com>
2021-07-07 20:59:51 +0530

    Day16

---
+++ Day16/my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {MemeCard , ChessBoard} from './App'
+
+
+ReactDOM.render(
+    <div className='container'>
+    <MemeCard/>
+    <ChessBoard/>
+    </div>,document.getElementById('root'));
