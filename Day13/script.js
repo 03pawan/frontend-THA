776154e5ac931e8f0d167b7e53ca7c764b1ac7f8
03pawan <pawankumar03012002@gmail.com>
2021-07-08 16:05:28 +0530

    Day13 changes

--- Day13/script.js
+++ Day13/script.js
@@ -7,7 +7,7 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
 
     let search = input.value;
-    if (/\s/.test(search) || search==NULL){
+    if (/\s/.test(search) || search==null){
         input.value="";
         return;
     }
@@ -23,6 +23,7 @@ btn.addEventListener('click', () => {
 
 function displayDetails(data){
     let div=document.createElement('div');
-    div.innerHTML='<br>'+ "Name: "+data.name+'<br>'+"Username: "+data.login+'<br>'+"Followers: "+data.followers+'<br>'+"Following: "+data.following+'<br>' +`<a href="${data.html_url}" target="_blank" rel="noopener noreferrer"> Check Profile here</a>`;
+    div.classList.add('details');
+    div.innerHTML= "Name: "+data.name+'<br>'+"Username: "+data.login+'<br>'+"Followers: "+data.followers+'<br>'+"Following: "+data.following+'<br>' +`<a href="${data.html_url}" target="_blank" rel="noopener noreferrer"> Check Profile here</a>`;
     container.append(div);
 }
