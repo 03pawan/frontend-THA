e7de5b075cfda08c7a9206f77e8e014797fef07a
03pawan <pawankumar03012002@gmail.com>
2021-07-08 20:28:32 +0530

    Day14

---
+++ Day14/script.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector('button');
+
+
+btn.addEventListener('click',()=>{
+    document.body.style.backgroundColor = '#9B72AA';
+});
+
+function mouseOver(){
+    document.body.style.backgroundColor = '#B5EAEA'
+}
+
+function mouseOut(){
+    document.body.style.backgroundColor = '#E6DDC6'
+}
