20067073eea69262c32e6b60e51522ed1660b6c2
03pawan <pawankumar03012002@gmail.com>
2021-07-09 16:23:55 +0530

    Day12 fixes

--- Day12/script.js
+++ Day12/script.js
@@ -1,54 +1,30 @@
 let btn = document.querySelector('button');
 let list = document.querySelector('.list');
 let todoList = document.querySelector('.todo')
-let checkBox=[];
-let newTodo=document.querySelector('.new-work');
-let toDoNum=0;
-let checked=[];
 
-var toDo = [];
+let newTodo=document.querySelector('.new-work');
 
 btn.addEventListener('click',()=>{
-    console.log(newTodo.value);
     if(newTodo.value.trim()=="")
         return null;
-    var newDiv={
-        work: newTodo.value,
-        check: false
-    }
-    newTodo.value="";
-    toDo.push(newDiv);
     let div = document.createElement("input");
     div.type='text';
     div.classList.add('todo');
-    div.value = toDo[toDoNum].work;
+    div.value = newTodo.value;
      list.appendChild(div);
     let div2 = document.createElement("input");
     div2.type='checkbox';
-  
     div2.classList.add('check');
-    div2.onclick=function(){checkboxClick(div2)};
-    
-   
+    div2.onclick=function(){checkboxClick(div)};
     list.appendChild(div2);
-   
-    toDoNum++;
-    checkBox = document.querySelectorAll('.check');
-    checked = document.querySelectorAll('.check').checked = true;
+    newTodo.value="";
     return;
 });
 
-
-checkBox.forEach((box=>{
-    box.addEventListener('click',()=>{
-        console.log("checked");
-    })
-}));
-
-function checkboxClick(div2){
-    if(div2.checked == 'false')
-        console.log(div2);
-    else
-        console.log(div2+"HI");
+function checkboxClick(div,div2){
+    div.style.textDecoration='line-through' ;   
+    setTimeout(()=>{
+        list.remove(div,div2);
+    },1000)
 }
 
