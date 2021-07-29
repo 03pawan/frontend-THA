10349fb4c780786ab377b0c53914c4bf32ae4049
03pawan <pawankumar03012002@gmail.com>
2021-07-29 17:55:47 +0530

    Day25

---
+++ Day25/my-app/src/App.js
@@ -0,0 +1,13 @@
+import Example from "./Example";
+import Example2 from "./Example2"
+
+function App() {
+  return (
+    <div className="App">
+      <Example/>
+      <Example2/>
+    </div>
+  );
+}
+
+export default App;
